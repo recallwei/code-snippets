e00c7ae3b782d07b2b60a4696aa07d547b12b831
Bruce <recall4056@gmail.com>
2023-05-17 18:29:26 +0800

    chore: update

---
+++ prettier/astro.js
@@ -0,0 +1,32 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSameLine: false,
+  bracketSpacing: true,
+  embeddedLanguageFormatting: 'auto',
+  endOfLine: 'lf',
+  htmlWhitespaceSensitivity: 'css',
+  jsxSingleQuote: false,
+  printWidth: 120,
+  proseWrap: 'preserve',
+  quoteProps: 'as-needed',
+  semi: false,
+  singleAttributePerLine: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'none',
+  useTabs: false,
+  astroAllowShorthand: false,
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro'
+      }
+    }
+  ],
+  plugins: [
+    require.resolve('prettier-plugin-astro')
+    // NOTE: Using ESLint to sort TailwindCSS class name
+    // require.resolve('prettier-plugin-tailwindcss')
+  ]
+}
