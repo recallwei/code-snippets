21ca2fae92094bf8824b147027d019e1bb1ec241
Bruce <recall4056@gmail.com>
2023-08-22 17:20:34 +0800

    feat: init docs project

---
+++ .vitepress/theme/index.ts
@@ -0,0 +1,13 @@
+import { h } from 'vue'
+import Theme from 'vitepress/theme'
+import './style.css'
+
+export default {
+  extends: Theme,
+  Layout: () => {
+    return h(Theme.Layout, null, {})
+  },
+  enhanceApp({ app, router, siteData }) {
+    // ...
+  }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Code Snippets
+
+⌨️ Some code snippets makes your life better!
+
+## License
+
+[MIT](/LICENSE) License &copy; 2023 [Bruce Song](https://github.com/recallwei)
